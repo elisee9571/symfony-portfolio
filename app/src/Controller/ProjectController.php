15052320db27eece4f42a3b4521190686c39b7b1
elisee9571 <elisee.desmarest@gmail.com>
2022-06-20 16:41:57 +0200

    paginattion + navbar dynamic

--- app/src/Controller/ProjectController.php
+++ app/src/Controller/ProjectController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Project;
 use App\Repository\ProjectRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,10 +20,14 @@ class ProjectController extends AbstractController
     }
 
     #[Route(name: 'index')]
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
-        $projects = $this->projectRepository->findAll();
-
+        $projects = $paginator->paginate(
+            $this->projectRepository->findAll(),
+            $request->query->getInt('page', 1),
+            6
+        );
+        
         return $this->render('project/index.html.twig', [
             'projects' => $projects,
         ]);
