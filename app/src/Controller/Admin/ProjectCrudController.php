e9eb011acec738317f33faa6590fc65bf985b116
elisee9571 <elisee.desmarest@gmail.com>
2022-06-24 15:21:10 +0200

    trad colonne project

--- app/src/Controller/Admin/ProjectCrudController.php
+++ app/src/Controller/Admin/ProjectCrudController.php
@@ -12,6 +12,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProjectCrudController extends AbstractCrudController
@@ -32,14 +33,15 @@ class ProjectCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            TextField::new('imageFile')->setFormType(VichImageType::class)->hideOnIndex(),
-            ImageField::new('file')->setBasePath('/uploads/realisations/')->onlyOnIndex(),
-            TextField::new('name'),
-            TextareaField::new('content')->hideOnIndex(),
+            TextField::new('imageFile', 'Image')->setFormType(VichImageType::class)->hideOnIndex(),
+            ImageField::new('file', 'Image')->setBasePath('/uploads/realisations/')->onlyOnIndex(),
+            TextField::new('name', 'Titre'),
+            TextareaField::new('content', 'Description')->hideOnIndex(),
             DateField::new('date'),
-            BooleanField::new('isVisible'),
+            BooleanField::new('isVisible', 'Visible'),
+            UrlField::new('link', 'Lien')->hideOnIndex(),
             SlugField::new('slug')->setTargetFieldName('name')->hideOnIndex(),
-            AssociationField::new('category'),
+            AssociationField::new('category', 'Catégories'),
         ];
     }
 
